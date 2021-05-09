dd34d3470b09171fc19957df3e795a5662c603fd
xavew <59637628+xavew@users.noreply.github.com>
2021-05-10 08:26:35 +1000

    Create little_battle.py

---
+++ little_battle.py
@@ -0,0 +1,16 @@
+import sys
+
+# Please implement this function according to Section "Read Configuration File"
+def load_config_file(filepath):
+  # It should return width, height, waters, woods, foods, golds based on the file
+  # Complete the test driver of this function in file_loading_test.py
+  width, height = 0, 0
+  waters, woods, foods, golds = [], [], [], [] # list of position tuples
+  
+  return width, height, waters, woods, foods, golds
+
+if __name__ == "__main__":
+  if len(sys.argv) != 2:
+    print("Usage: python3 little_battle.py <filepath>")
+    sys.exit()
+  width, height, waters, woods, foods, golds = load_config_file(sys.argv[1])
