813bdb894f202fe315deb7d5f6bce7b892142828
xavew <59637628+xavew@users.noreply.github.com>
2021-05-25 09:50:04 +1000

    Initial Player Recruit

--- battle.py
+++ battle.py
@@ -2,69 +2,79 @@ import sys
 from player import Player
 
 class Battle:
-  def __init__(self,width, height):
-    self.board = []
-    for row in range(height):
-        list = []
-        for column in range(width):
-            list.append("  ")
-        self.board.append(list)
-    print("Game Started: Little Battle! (enter QUIT to quit the game)")
+    def __init__(self,width, height):
+        self.board = []
+        for row in range(height):
+            list = []
+            for column in range(width):
+                list.append("  ")
+            self.board.append(list)
+        print("Game Started: Little Battle! (enter QUIT to quit the game)")
 
-  def setResources(self,list,resource):
-      for i in list:
-        if resource == "water":
-            self.board[i[0]][i[1]] = "~~" 
-        if resource == "food":
-            self.board[i[0]][i[1]] = "FF" 
-        if resource == "gold":
-            self.board[i[0]][i[1]] = "GG" 
-        if resource == "wood":
-            self.board[i[0]][i[1]] = "WW" 
-      
-  def setPos(self,cords,name):
-      x = cords[0]
-      y = cords[1]
+    def setResources(self,list,resource):
+        for i in list:
+            if resource == "water":
+                self.board[i[0]][i[1]] = "~~" 
+            if resource == "food":
+                self.board[i[0]][i[1]] = "FF" 
+            if resource == "gold":
+                self.board[i[0]][i[1]] = "GG" 
+            if resource == "wood":
+                self.board[i[0]][i[1]] = "WW" 
+        
+    def setPos(self,cords,name):
+        x = cords[0]
+        y = cords[1]
 
-      self.board[x][y] = name
+        self.board[x][y] = name
+
+    def checkSurrounds(self,cords):
+        flag = False
+        x = cords[0]
+        y = cords[1]
+        if self.board[x+1][y] != "  " and self.board[x-1][y] != "  " and self.board[x][y+1] != "  " and self.board[x][y-1] != "  ":
+            flag = True
+        return flag
+
+    def printGameState(self,width,height):
+        print("\nPlease check the battlefield, commander.")
+        
+        # Print top line
+        map = "  X"
+        for i in range(width):
+            map += "0{} ".format(i)
+        map = map[:-1]
+        map += "X\n"
+
+        # Print Y line
+        map += " Y+"
+        for i in range(width):
+            map += "---"
+        map = map[:-1]
+        map += "+\n"
+
+        # Print each row
+        for i in range(height):
+            map += "0{}".format(i)
+            for j in range(width):
+                map += "|{}".format(self.board[j][i])
+            map += "|\n"
+
+        # Print bottom Y line    
+        map += " Y+"
+        for i in range(width):
+            map += "---"
+        map = map[:-1]
+        map += "+"
+
+        print(map)
+
+    def move(self):
+        pass
+
+    def check_finished(self):
+        pass
 
 
-  def printGameState(self,width,height):
-    print("\nPlease check the battlefield, commander.")
-    
-    # Print top line
-    map = "  X"
-    for i in range(width):
-        map += "0{} ".format(i)
-    map = map[:-1]
-    map += "X\n"
 
-    # Print Y line
-    map += " Y+"
-    for i in range(width):
-        map += "---"
-    map = map[:-1]
-    map += "+\n"
-
-    # Print each row
-    for i in range(height):
-        map += "0{}".format(i)
-        for j in range(width):
-            map += "|{}".format(self.board[j][i])
-        map += "|\n"
-
-    # Print bottom Y line    
-    map += " Y+"
-    for i in range(width):
-        map += "---"
-    map = map[:-1]
-    map += "+"
-
-    print(map)
-
-  def move(self):
-    pass
-
-  def check_finished(self):
-    pass
 
--- player.py
+++ player.py
@@ -19,3 +19,32 @@ class Player():
         else:
             self.armyPos.append((width-2,height-2))
             self.game.setPos((width-2,height-2),"H2")
+
+    def recruit(self):
+        print("+++Player {}'s Stage: Recruit Armies+++".format(self.num))
+        print("[Your Asset: Wood - x{} Food - x{} Gold - x{}]".format(self.inv[0],self.inv[1],self.inv[2]))
+        self.checkRecruitValid()
+
+
+
+    def checkRecruitValid(self):
+        # Check map valid
+        home = self.armyPos[0]
+        if self.game.checkSurrounds(home) == True:
+            print("No place to recruit new armies.")
+            return True
+
+        # Check resources valid
+        if (self.inv[0] != 0 and self.inv[1] != 0) or (self.inv[1] != 0 and self.inv[2] != 0) or (self.inv[0] != 0 and self.inv[2] != 0):
+            print("No resources to recruit armies.")
+            return True
+
+
+        # There are four types of soldiers: 
+        # Spearman (S) costs 1W, 1F; 
+        # Archer (A) costs 1W, 1G; 
+        # Knight (K) costs 1F, 1G, 
+
+        # [1,1,0]
+        # [0,1,1]
+        # [1,0,1]
